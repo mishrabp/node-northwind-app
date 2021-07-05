6e3da07cf4d252a06184984fe8e0e9433bc2ffbe
mishrab <bibhup_mishra@yahoo.com>
2021-07-05 11:09:43 -0500

    pipeline file organizing

--- setprodenv.cmd
+++ setprodenv.cmd
@@ -5,4 +5,4 @@ set MYNWAPP_SessionKey=sessionkey1
 set MYNWAPP_GEOCODER_API_KEY=AIzaSyAFN7pm1QA20ojk8CA2tQnXzOHB1ryRGtM
 set MYNWAPP_ERRORLOG=true
 set MYNWAPP_TRACKINGLOG=true
-set MYNWAPP_MONGO_URI="mongodb://mongoadmin:passw0rd!@devopsmasterlinuxvm.centralus.cloudapp.azure.com:27017/northwind?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+set MYNWAPP_MONGO_URI="mongodb://mongoadmin:passw0rd!@devopsmasterlinuxvm.centralus.cloudapp.azure.com:9003/northwind?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false"
